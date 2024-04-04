b13d6ca5bd592626244c78e1844d6d3a7318ee16
Gayrat <gayratshirinboyev6707@gmail.com>
2024-04-04 12:15:45 +0500

    add product

--- src/backend/model/product/Product.java
+++ src/backend/model/product/Product.java
@@ -1,7 +1,59 @@
 package backend.model.product;
 
-public class Product {
 
 
+import backend.enums.ProductCategory;
+import backend.model.BaseModel;
 
+import java.math.BigDecimal;
+
+public class Product extends BaseModel {
+    private String name;
+    private BigDecimal price;
+    private int count;
+    private ProductCategory category;
+
+    public Product(String name, BigDecimal price, int count, ProductCategory category) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ProductCategory category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return  name + " " + price+ " "  + count +" " + category;
+    }
 }
